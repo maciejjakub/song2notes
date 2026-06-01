fb27fa895fb160e70371813bfd690bebbaca04b8
Maciej Miliszewski <maciejmiliszewski@gmail.com>
2026-06-01 12:24:49 +0200

    switch config to not tmp directory, to make it REALLY persistent

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    UPLOAD_DIR: str = "/tmp/song2notes/uploads"
-    OUTPUT_DIR: str = "/tmp/song2notes/outputs"
+    UPLOAD_DIR: str = "../samples/uploads"
+    OUTPUT_DIR: str = "../samples/outputs"
     DB_PATH: str = "song2notes.db"
     MAX_FILE_SIZE_MB: int = 50
 
